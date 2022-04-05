06440aed0eefcd751681b8dd831a0209ea0253d2
Diego Zaccariotto <diego.zaccariotto@outlook.com>
2022-04-05 22:33:49 +0200

    http_server_golang - added Users db code

--- http_server_golang/go.mod
+++ http_server_golang/go.mod
@@ -1,3 +1,5 @@
 module github.com/dzacca/go/http_server_golang
 
 go 1.18
+
+require github.com/google/uuid v1.3.0 // indirect
--- http_server_golang/internal/database/database.go
+++ http_server_golang/internal/database/database.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"encoding/json"
+	"errors"
+	"log"
 	"os"
 	"time"
 )
@@ -12,7 +14,7 @@ type Client struct {
 	path string
 }
 
-func NewClient(path string) {
+func NewClient(path string) Client {
 	return Client{path: path}
 }
 
@@ -40,7 +42,7 @@ type Post struct {
 
 // EnsureDB creates the db file if it doesn't exist
 func (c Client) EnsureDB() error {
-	data, err := os.ReadFile(c.path)
+	_, err := os.ReadFile(c.path)
 	if err != nil {
 		return c.createDB()
 	}
@@ -56,9 +58,122 @@ func (c Client) createDB() error {
 	if err != nil {
 		return err
 	}
-	err := os.WriteFile(c.path, dat, 0644)
+	err = os.WriteFile(c.path, dat, 0644)
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+// updateDB
+func (c Client) updateDB(db databaseSchema) error {
+	dat, err := json.Marshal(db)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(c.path, dat, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// readDB
+func (c Client) readDB() (databaseSchema, error) {
+
+	data, err := os.ReadFile(c.path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := databaseSchema{}
+	err = json.Unmarshal(data, &db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db, nil
+}
+
+// CreateUser
+func (c Client) CreateUser(email, password, name string, age int) (User, error) {
+	db, err := c.readDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := User{
+		CreatedAt: time.Now().UTC(),
+		Email:     email,
+		Password:  password,
+		Name:      name,
+		Age:       age,
+	}
+
+	db.Users[email] = user
+
+	err = c.updateDB(db)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+// UpdateUser
+func (c Client) UpdateUser(email, password, name string, age int) (User, error) {
+	db, err := c.readDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := User{
+		Email:    email,
+		Password: password,
+		Name:     name,
+		Age:      age,
+	}
+
+	_, ok := db.Users[email]
+	if !ok {
+		return user, errors.New("user doesn't exist")
+	}
+
+	db.Users[email] = user
+
+	err = c.updateDB(db)
+	if err != nil {
+		u := User{}
+		return u, err
+	}
+	return user, nil
+
+}
+
+// GetUser
+func (c Client) GetUser(email string) (User, error) {
+	db, err := c.readDB()
+	if err != nil {
+		u := User{}
+		return u, errors.New("User not found")
+	}
+	if _, ok := db.Users[email]; !ok {
+		u := User{}
+		return u, errors.New("User not found")
+	}
+	return db.Users[email], nil
+}
+
+// DeleteUser
+func (c Client) DeleteUser(email string) error {
+	db, err := c.readDB()
+	if err != nil {
+		return err
+	}
+	delete(db.Users, email)
+	err = c.updateDB(db)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
